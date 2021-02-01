0a8a6be008a573954f22839576febb407872d156
nimisaya <nimisaya@outlook.com>
2021-02-01 15:56:09 +1100

    Link Homepage to art

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,7 +1,14 @@
+import styles from "./Home.module.css";
+
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
     <>
-      <h1>Home</h1>
+      <h1>Generate unique art</h1>
+      <Link to="/art/mathfun" className={styles.sketchlink}>
+        Get started
+      </Link>
     </>
   ); // return
 }; // Home
