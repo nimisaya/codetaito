36d592d543bec7fc5d469d6139368c7b5aec957d
nimisaya <nimisaya@outlook.com>
2021-02-01 18:03:17 +1100

    Animate a circle for testing

--- src/components/art/Mathfun.js
+++ src/components/art/Mathfun.js
@@ -1,19 +1,38 @@
 import * as p5 from "p5";
 import useDOMControl from "../../customHooks/useDOMControl";
-import styles from "./art.module.css";
+import styles from "./Art.module.css";
 
 const Mathfun = () => {
   // Construct canvas node
   const p5Fn = (p5Ref) => {
     const sketch = (p) => {
+      let x, y;
+
       p.setup = () => {
         p.createCanvas(400, 400);
         p.background(0);
+
+        x = p.width / 2;
+        y = p.height;
       };
 
       p.draw = () => {
-        p.fill(255);
-        p.ellipse(p.width / 2, p.height / 2, 200);
+        p.background(200);
+
+        // Draw a circle
+        p.stroke(50);
+        p.fill(100);
+        p.ellipse(x, y, 24, 24);
+
+        // Jiggling randomly on the horizontal axis
+        x = x + p.random(-1, 1);
+        // Moving up at a constant speed
+        y = y - 1;
+
+        // Reset to the bottom
+        if (y < 0) {
+          y = p.height;
+        }
       };
     };
 
@@ -22,7 +41,7 @@ const Mathfun = () => {
 
   return (
     <div>
-      <h1>Mathfun</h1>
+      <h1>Math asd</h1>
       {/* Note: to style canvas use 'canvas' in css */}
       <div className={styles.artboard}>{useDOMControl(p5Fn)}</div>
     </div>
