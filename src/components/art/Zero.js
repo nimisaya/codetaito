b45f54c2841572ef023a7c8bd6efad3c215757c6
nimisaya <nimisaya@outlook.com>
2021-02-02 15:10:00 +1100

    Set dimensions of canvas using dropdown

--- src/components/art/Zero.js
+++ src/components/art/Zero.js
@@ -5,18 +5,30 @@ import styles from "./Art.module.css";
 
 import Navbar from "../navbar/Navbar";
 
+const DEFAULT_WIDTH = 600;
+const DEFAULT_HEIGHT = 400;
+
 const Zero = () => {
   // Colour options displayed in dropdown list
   const COLORS = ["black", "blue", "hotpink", "red", "green"];
+
+  // Dimensions options displayed in dropdown list
+  const DIMENSIONS = ["Default", "Instagram", "Zoom"];
 
   // canvasRef.current holds the canvas DOM node.
   const canvasRef = useRef();
 
   const title = "zero";
-  const [width, setWidth] = useState(400);
-  const [height, setHeight] = useState(400);
+  const [width, setWidth] = useState(DEFAULT_WIDTH);
+  const [height, setHeight] = useState(DEFAULT_HEIGHT);
+
   // const [color, setColour] = useState("black");
-  const [color, ColorDropdown] = useDropdown("Colour", "#000000", COLORS);
+  const [color, ColorDropdown] = useDropdown("Colour", COLORS[0], COLORS);
+  const [dimensions, DimensionsDropdown] = useDropdown(
+    "Dimensions",
+    DIMENSIONS[0],
+    DIMENSIONS
+  );
 
   useLayoutEffect(() => {
     const canvas = canvasRef.current;
@@ -25,6 +37,8 @@ const Zero = () => {
     const context = canvas.getContext("2d");
 
     // Set canvas dimensions
+    updateWidthHeight(dimensions, setWidth, setHeight);
+    // updateWidthHeight(dimensions);
     canvas.width = width;
     canvas.height = height;
 
@@ -51,7 +65,7 @@ const Zero = () => {
 
     // render graphics
     render();
-  }, [width, height, color]); // useEffect
+  }, [width, height, color, dimensions]); // useEffect
 
   const saveFileBtn = () => {
     // console.log(canvasRef);
@@ -77,41 +91,12 @@ const Zero = () => {
           </li>
         </ul>
 
-        <label htmlFor="width">
-          Width
-          <br />
-          <input
-            id="width"
-            value={width}
-            placeholder="Width"
-            onChange={(e) => setWidth(e.target.value)}
-          />
-        </label>
-        <br />
-        <br />
-        <label htmlFor="height">
-          Height
-          <br />
-          <input
-            id="height"
-            value={height}
-            placeholder="height"
-            onChange={(e) => setHeight(e.target.value)}
-          />
-        </label>
-        <br />
-        <br />
         <ColorDropdown />
-        {/* <label htmlFor="colour">
-          Colour
-          <br />
-          <input
-            id="colour"
-            value={color}
-            placeholder="colour"
-            onChange={(e) => setColour(e.target.value)}
-          />
-        </label> */}
+        <br />
+        <DimensionsDropdown />
+        <p>
+          Width: {width}, height: {height}
+        </p>
         <br />
         <br />
 
@@ -162,3 +147,20 @@ const saveFile = () => {
     console.log(link);
   };
 }; // saveFile
+
+const updateWidthHeight = (dimensions, setWidth, setHeight) => {
+  switch (dimensions) {
+    case "Instagram":
+      setWidth(1080);
+      setHeight(1080);
+      break;
+    case "Zoom":
+      setWidth(1920);
+      setHeight(1080);
+      break;
+    default:
+      setWidth(DEFAULT_WIDTH);
+      setHeight(DEFAULT_HEIGHT);
+      break;
+  }
+};
