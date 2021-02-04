b948383666decbe65b1b517bda0e967845610b4b
nimisaya <nimisaya@outlook.com>
2021-02-04 22:56:47 +1100

    Temporarily remove homepage

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,20 +3,22 @@ import "./App.modules.css";
 import React from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
-import Home from "../home/Home";
+// import Home from "../home/Home";
 import Zero from "../art/Zero";
 import ZeroAudio from "../art/ZeroAudio";
-import Artboard from "../artboard/Artboard";
+// import Artboard from "../artboard/Artboard";
+import Navbar from "../navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        {/* <Route path="/" component={Artboard} /> */}
+        <Route path="/" component={Navbar} />
         <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/art/zero" component={Zero} />
-          <Route path="/art/mikedrop" component={ZeroAudio} />
-          <Route exact path="/art" component={Artboard} />
+          <Route exact path="/" component={Zero} />
+          {/* <Route path="/art/zero" component={Zero} /> */}
+          <Route path="/mikedrop" component={ZeroAudio} />
         </Switch>
       </Router>
       <footer>
