773f1a25f80363095501dd453bd780022f3be431
nimisaya <nimisaya@outlook.com>
2021-02-04 22:15:53 +1100

    Add link to zero audio

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -5,6 +5,7 @@ import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
 import Home from "../home/Home";
 import Zero from "../art/Zero";
+import ZeroAudio from "../art/ZeroAudio";
 import Artboard from "../artboard/Artboard";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/art/zero" component={Zero} />
+          <Route path="/art/mikedrop" component={ZeroAudio} />
           <Route exact path="/art" component={Artboard} />
         </Switch>
       </Router>
