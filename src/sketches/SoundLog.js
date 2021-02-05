54018d08e02ebce55d214dacdf7139def0e52657
nimisaya <nimisaya@outlook.com>
2021-02-05 12:33:23 +1100

    Add comment

--- src/sketches/SoundLog.js
+++ src/sketches/SoundLog.js
@@ -9,13 +9,14 @@ const SoundLog = (context, canvas, a) => {
   context.beginPath();
 
   const shape = (theta) => {
-    return 1 - Math.cos(a * theta) * Math.sin(a * theta);
+    // return 1 - Math.cos(a * theta) * Math.sin(a * theta);
     // return a - Math.cos(a * theta) * Math.sin(a * theta);
-    // return 1 - Math.cos(theta) * Math.sin(a * theta);
+    return 1 - Math.cos(theta) * Math.sin(a * theta);
   };
 
   // Construct an array of x points
-  // theta is the index of current item in the array/100
+  // theta (angle) is the index of current item in the array/100
+  // Keys returns a new Array Iterator object that contains the keys for each index in the array.
   Array.from(Array(points).keys()).forEach((idx) => {
     const theta = idx * 0.01;
     const r = canvas.width * (1 / 5) * shape(theta);
